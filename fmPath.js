405a12a1ddd60b43a665a4c0acd4817dbf062bad
Simon Chauvin <chauvin.simon@gmail.com>
2013-02-09 14:04:28 +0100

    Added an object for making paths

---
+++ fmPath.js
@@ -0,0 +1,31 @@
+/**
+ * Object representing a path.
+ * @author Simon Chauvin
+ */
+FMENGINE.fmPath = function () {
+    "use strict";
+    var that = {};
+
+    /**
+     * Waypoints constituing the path.
+     */
+    that.waypoints = [];
+
+    /**
+     * Add a waypoint to the path.
+     * @param {int} x x position.
+     * @param {int} y y position.
+     */
+    that.add = function (x, y) {
+        
+    };
+
+    /**
+     * Remove a waypoint from the path.
+     */
+    that.remove = function () {
+        
+    };
+
+    return that;
+};

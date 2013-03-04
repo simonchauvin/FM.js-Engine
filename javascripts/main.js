69e8bc97e25e2699f010f9931bd0a5f0077ccdf2
Simon Chauvin <chauvin.simon@gmail.com>
2013-03-04 19:12:34 +0100

    Updated the website layout and images

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,31 @@
-console.log('This would be the main JS file.');
+window.onload = function() {
+    if (document.getElementById("home_content").style.visibility == 'hidden') {
+        document.getElementsByClassName("home")[0].style.backgroundPosition = "100px 0px";
+        document.getElementsByClassName("home")[0].style.height = "62px";
+    }
+    
+    if (document.getElementById("features_content")) {
+        document.getElementsByClassName("features")[0].style.backgroundPosition = "-100px 0px";
+        document.getElementsByClassName("features")[0].style.height = "62px";
+    }
+    
+    if (document.getElementById("gallery_content")) {
+        document.getElementsByClassName("gallery")[0].style.backgroundPosition = "-100px 0px";
+        document.getElementsByClassName("gallery")[0].style.height = "62px";
+    }
+    
+    if (document.getElementById("download_content")) {
+        document.getElementsByClassName("download")[0].style.backgroundPosition = "-100px 0px";
+        document.getElementsByClassName("download")[0].style.height = "62px";
+    }
+    
+    if (document.getElementById("community_content")) {
+        document.getElementsByClassName("community")[0].style.backgroundPosition = "-100px 0px";
+        document.getElementsByClassName("community")[0].style.height = "62px";
+    }
+    
+    if (document.getElementById("help_content")) {
+        document.getElementsByClassName("help")[0].style.backgroundPosition = "-100px 0px";
+        document.getElementsByClassName("help")[0].style.height = "62px";
+    }
+}

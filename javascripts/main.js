735a1ecb186e0401e2561b4918f6c9f86dedafc8
Simon Chauvin <chauvin.simon@gmail.com>
2013-03-07 19:01:56 +0100

    Modified to use dynamic script page display

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,30 +1,31 @@
-window.onload = function() {
-    if (document.getElementById("home_content").style.visibility == 'hidden') {
+window.onload = function () {
+    console.log(document.getElementById("home_content").style.display);
+    if (document.getElementById("home_content").style.display === 'block') {
         document.getElementsByClassName("home")[0].style.backgroundPosition = "100px 0px";
         document.getElementsByClassName("home")[0].style.height = "62px";
     }
     
-    if (document.getElementById("features_content")) {
+    if (document.getElementById("features_content").style.display === "block") {
         document.getElementsByClassName("features")[0].style.backgroundPosition = "-100px 0px";
         document.getElementsByClassName("features")[0].style.height = "62px";
     }
     
-    if (document.getElementById("gallery_content")) {
+    if (document.getElementById("gallery_content").style.display === "block") {
         document.getElementsByClassName("gallery")[0].style.backgroundPosition = "-100px 0px";
         document.getElementsByClassName("gallery")[0].style.height = "62px";
     }
     
-    if (document.getElementById("download_content")) {
+    if (document.getElementById("download_content").style.display === "block") {
         document.getElementsByClassName("download")[0].style.backgroundPosition = "-100px 0px";
         document.getElementsByClassName("download")[0].style.height = "62px";
     }
     
-    if (document.getElementById("community_content")) {
+    if (document.getElementById("community_content").style.display === "block") {
         document.getElementsByClassName("community")[0].style.backgroundPosition = "-100px 0px";
         document.getElementsByClassName("community")[0].style.height = "62px";
     }
     
-    if (document.getElementById("help_content")) {
+    if (document.getElementById("help_content").style.display === "block") {
         document.getElementsByClassName("help")[0].style.backgroundPosition = "-100px 0px";
         document.getElementsByClassName("help")[0].style.height = "62px";
     }

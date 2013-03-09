92e31363d8d92f644805448a822ae98cbc46dc01
Simon Chauvin <chauvin.simon@gmail.com>
2013-03-09 13:03:39 +0100

    Updated style and script

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,32 +1,18 @@
+var displayedPage = "home_content";
+var highlightedButton = "home";
+
 window.onload = function () {
-    console.log(document.getElementById("home_content").style.display);
-    if (document.getElementById("home_content").style.display === 'block') {
-        document.getElementsByClassName("home")[0].style.backgroundPosition = "100px 0px";
-        document.getElementsByClassName("home")[0].style.height = "62px";
-    }
-    
-    if (document.getElementById("features_content").style.display === "block") {
-        document.getElementsByClassName("features")[0].style.backgroundPosition = "-100px 0px";
-        document.getElementsByClassName("features")[0].style.height = "62px";
-    }
-    
-    if (document.getElementById("gallery_content").style.display === "block") {
-        document.getElementsByClassName("gallery")[0].style.backgroundPosition = "-100px 0px";
-        document.getElementsByClassName("gallery")[0].style.height = "62px";
-    }
-    
-    if (document.getElementById("download_content").style.display === "block") {
-        document.getElementsByClassName("download")[0].style.backgroundPosition = "-100px 0px";
-        document.getElementsByClassName("download")[0].style.height = "62px";
-    }
-    
-    if (document.getElementById("community_content").style.display === "block") {
-        document.getElementsByClassName("community")[0].style.backgroundPosition = "-100px 0px";
-        document.getElementsByClassName("community")[0].style.height = "62px";
-    }
-    
-    if (document.getElementById("help_content").style.display === "block") {
-        document.getElementsByClassName("help")[0].style.backgroundPosition = "-100px 0px";
-        document.getElementsByClassName("help")[0].style.height = "62px";
-    }
-}
+    document.getElementsByClassName("home")[0].style.backgroundPosition = "100px 0px";
+    document.getElementsByClassName("home")[0].style.height = "62px";
+};
+
+var menuClick = function (pageToDisplay, buttonToHighlight) {
+    document.getElementsByClassName(highlightedButton)[0].style.backgroundPosition = "0px 0px";
+    document.getElementsByClassName(highlightedButton)[0].style.height = "40px";
+    document.getElementById(displayedPage).style.display = 'none';
+    document.getElementById(pageToDisplay).style.display = 'block';
+    document.getElementsByClassName(buttonToHighlight)[0].style.backgroundPosition = "-100px 0px";
+    document.getElementsByClassName(buttonToHighlight)[0].style.height = "62px";
+    displayedPage = pageToDisplay;
+    highlightedButton = buttonToHighlight;
+};

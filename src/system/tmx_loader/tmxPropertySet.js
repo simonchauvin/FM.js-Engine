d4b5e70bbf53f88200c8aebb46c4b3e05a58311b
Simon Chauvin <chauvin.simon@gmail.com>
2013-12-02 21:00:56 +0100

    Moved tmx loader to the system folder

---
+++ src/system/tmx_loader/tmxPropertySet.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+var tmxPropertySet = function () {
+    "use strict";
+    var that = [];
+
+    /**
+     * 
+     */
+    that.add = function (propertyNode) {
+        var key = propertyNode.getAttribute("name"),
+            value = propertyNode.getAttribute("value");
+        that[key] = value;
+    };
+
+    return that;
+};

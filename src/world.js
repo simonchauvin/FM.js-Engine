5d437300493077ae71132f9201bb04c068962724
Simon Chauvin <chauvin.simon@gmail.com>
2014-01-11 14:58:41 +0100

    Updated documentation

--- src/world.js
+++ src/world.js
@@ -1,6 +1,9 @@
+/*global FM*/
 /**
  * World represent the concrete space of the game.
  * @class world
+ * @param {int} pWidth width of the tile map in pixels
+ * @param {int} pHeight height of the tile map in pixels
  * @author Simon Chauvin
  */
 FM.world = function (pWidth, pHeight) {
@@ -8,12 +11,22 @@ FM.world = function (pWidth, pHeight) {
     var that = FM.rectangle(0, 0, pWidth, pHeight),
         /**
          * Tile maps of the world.
+         * @private
+         * @type Array
+         * @name world#tileMaps
+         * @field
          */
         tileMaps = [];
 
     /**
      * Add a tile map to the current world.
+     * @public
+     * @name world#loadTileMap
+     * @function
      * @param {tileMap} pTileMap tile map to add.
+     * @param {tmxMap} pMap tmxMap containing the tile map data.
+     * @param {string} pLayerName name of the layer of the tile map.
+     * @param {string} pTileSetName name of the tile set to use.
      */
     that.loadTileMap = function (pTileMap, pMap, pLayerName, pTileSetName) {
         pTileMap.load(pMap.getLayer(pLayerName).toCsv(pMap.getTileSet(pTileSetName)));
@@ -22,8 +35,11 @@ FM.world = function (pWidth, pHeight) {
 
     /**
      * Retrieve the tile map from the given type.
+     * @public
+     * @name world#getTileMapFromType
+     * @function
      * @param {objectType} pType the type of the tile map to retrieve.
-     * @return {tileMap} the tile map corresponding to the given type.
+     * @return {tileMap} the tile map corresponding to the given type or null if none is found.
      */
     that.getTileMapFromType = function (pType) {
         var i, tileMap;
@@ -38,7 +54,10 @@ FM.world = function (pWidth, pHeight) {
 
     /**
      * Check if a tile map allow collisions.
-     * @return {Boolean} Whether there is a tile map with potential collisions.
+     * @public
+     * @name world#hasTileCollisions
+     * @function
+     * @return {Boolean} Whether there is a tile map with potential collisions or not.
      */
     that.hasTileCollisions = function () {
         var i;
@@ -51,7 +70,10 @@ FM.world = function (pWidth, pHeight) {
     };
 
     /**
-    * Destroy the world and its objects
+    * Destroy the world and its objects.
+    * @public
+    * @name world#destroy
+    * @function
     */
     that.destroy = function () {
         that = null;
